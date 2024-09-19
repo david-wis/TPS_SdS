dd0e9b2ba6bdcfc5d8682359ff9771ea48bed43e
davidwis <davidwis000@gmail.com>
2024-09-19 17:27:06 -0300

    Minor corrections in plots

--- TP_3/src/Main.java
+++ TP_3/src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        staticObstacle();
-        movingObstacle();
+        staticObstacle();
+//        movingObstacle();
     }
 
     public static void staticObstacle(){
--- TP_3/temperature_analysis.py
+++ TP_3/temperature_analysis.py
@@ -33,6 +33,7 @@ ENERGY_UNIT = "J"
 def plot(xs, ys, x_label, y_label, filename):
     fig, ax = plt.subplots()
     plt.ticklabel_format(style='sci', axis='both', scilimits=(-5,5))
+    ax.plot(xs, ys)
     ax.scatter(xs, ys)
     ax.set_ylim((0, 1.1 * max(ys)))
     ax.set_xlabel(x_label)
@@ -46,12 +47,17 @@ def plot_aggregated(xss, yss, ls, x_label, y_label, filename):
     fig, ax = plt.subplots()
     plt.ticklabel_format(style='sci', axis='both', scilimits=(-5,5))
     for xs, ys, l in zip(xss, yss, ls):
-        ax.scatter(xs, ys, label=l, s=10)
+        ax.plot(xs, ys, label=l)
+        ax.scatter(xs, ys, label=l)
     ax.set_ylim((0, 1.1 * max([max(ys) for ys in yss])))
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.yaxis.set_major_formatter(formatter)
     ax.legend()
+    # remove legend for scatter
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles[::2], labels=labels[::2])
+
     plt.tight_layout()
     plt.savefig(f"{BASE_PATH}/{filename}.png")
     plt.close()
@@ -117,7 +123,7 @@ def analyze_velocity(v):
             return total_v
         wall_pressures[idx] += 2 * MASS * get_velocity_by_wall(x, y, vx, vy, RADIUS, dirs) / (DT * 4 * L)
 
-    plot([DT * i for i in range(len(wall_pressures))], list(wall_pressures.values()), f"Tiempo ({TIME_UNIT})",f"Presión sobre las paredes ({PRESSURE_UNIT})",f"{v}/wall_pressure")
+    # plot([DT * i for i in range(len(wall_pressures))], list(wall_pressures.values()), f"Tiempo ({TIME_UNIT})",f"Presión sobre las paredes ({PRESSURE_UNIT})",f"{v}/wall_pressure")
 
     #OBSTACLE==================================================================================================================================
     obs_pressures = {}
@@ -140,7 +146,14 @@ def analyze_velocity(v):
        if id not in obs_first_collisions.keys():
            obs_first_collisions[id] = t
 
-    plot([DT * i for i in range(len(obs_pressures))], list(obs_pressures.values()), f"Tiempo ({TIME_UNIT})",f"Presión sobre el obstáculo ({PRESSURE_UNIT})", f"{v}/obs_pressure")
+    # plot([DT * i for i in range(len(wall_pressures))], list(wall_pressures.values()), f"Tiempo ({TIME_UNIT})",f"Presión sobre las paredes ({PRESSURE_UNIT})",f"{v}/wall_pressure")
+    # plot([DT * i for i in range(len(obs_pressures))], list(obs_pressures.values()), f"Tiempo ({TIME_UNIT})",f"Presión sobre el obstáculo ({PRESSURE_UNIT})", f"{v}/obs_pressure")
+
+    plot_aggregated([[DT * i for i in range(len(wall_pressures))], [DT * i for i in range(len(obs_pressures))]],
+                    [list(wall_pressures.values()), list(obs_pressures.values())], ["Paredes", "Obstáculo"], f"Tiempo ({TIME_UNIT})",f"Presión ({PRESSURE_UNIT})",f"{v}/pressure")
+
+
+
     first_times = obs_first_collisions.values()
     first_times = sorted(first_times)
     first_collisions = [i for i in range(len(first_times))]
@@ -149,7 +162,7 @@ def analyze_velocity(v):
     all_times = [e[0] for e in obst_evs]
     plot(all_times, all_collisions, f"Tiempo ({TIME_UNIT})", "Cantidad de colisiones", f"{v}/all_collisions", )
 
-    time_10000 = obst_evs[1000][0]
+    time_1000 = obst_evs[1000][0]
     time_half = first_times[len(first_times) // 2]
     time_all = first_times[-1]
     return (
@@ -157,7 +170,7 @@ def analyze_velocity(v):
             wall_pressures, obs_pressures,
             first_times, first_collisions,
             all_times, all_collisions,
-            time_10000, time_half, time_all
+            time_1000, time_half, time_all
     )
 
 
@@ -195,8 +208,8 @@ if __name__ == "__main__":
     plot_aggregated(ftss, fcss, velocities, f"Tiempo ({TIME_UNIT})", "Cantidad de primeras colisiones", f"first_collisions")
     plot_aggregated(atss, acss, velocities, f"Tiempo ({TIME_UNIT})", "Cantidad de colisiones", f"all_collisions", )
 
-    plot(temperatures, pressures, f"Energía cinética del sistema ({ENERGY_UNIT})",f"Presión del sistema ({ENERGY_UNIT})","temperature_pressure")
-    plot(temperatures, times_10000, f"Energía cinética del sistema ({ENERGY_UNIT})",f"Tiempo para 10000 colisiones ({TIME_UNIT})","temperature_time_10000")
-    plot(temperatures, times_half, f"Energía cinética del sistema ({ENERGY_UNIT})",f"Tiempo para el 50% de las primeras partículas ({TIME_UNIT})","temperature_time_half")
+    plot(temperatures, pressures, f"Energía cinética promedio del sistema ({ENERGY_UNIT})",f"Presión del sistema ({ENERGY_UNIT})","temperature_pressure")
+    plot(temperatures, times_10000, f"Energía cinética promedio del sistema ({ENERGY_UNIT})",f"Tiempo para 1000 colisiones ({TIME_UNIT})","temperature_time_10000")
+    plot(temperatures, times_half, f"Energía cinética promedio del sistema ({ENERGY_UNIT})",f"Tiempo para el 50% de las primeras partículas ({TIME_UNIT})","temperature_time_half")
     plot(temperatures, times_all, f"Energía cinética del sistema ({ENERGY_UNIT})",f"Tiempo para el 100% de las primeras partículas ({TIME_UNIT})","temperature_time_all")
 
